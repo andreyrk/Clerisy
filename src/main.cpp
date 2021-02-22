801783be309d9c825f858f68929bbf53db693078
Andrey Érick <dev.andreyrk@gmail.com>
2021-02-22 08:59:00 -0300

    Set app and installer icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-#include <QIcon>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQuickStyle>
-#include <QSysInfo>
 
 int main(int argc, char *argv[])
 {
     qputenv("QML_XHR_ALLOW_FILE_READ", QByteArray(1, 1));
 
     QGuiApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":/Icon/App.png"));
-
     QQmlApplicationEngine engine;
     engine.load("qrc:/UI/App.qml");
 
